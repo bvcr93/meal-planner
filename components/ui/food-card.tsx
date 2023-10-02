67c05bd47c363d20d9a4a49a12678ce1ff32fcfe
bvcr93 <darelbavcar1@gmail.com>
2023-10-02 16:42:31 +0200

    changed spinner style, cards

--- components/ui/food-card.tsx
+++ components/ui/food-card.tsx
@@ -58,15 +58,15 @@ export default function FoodCard({
 
   function getColorBasedOnId(id: string) {
     const colors = [
-      "bg-red-200",
-      "bg-green-200",
-      "bg-blue-200",
-      "bg-yellow-200",
-      "bg-pink-200",
-      "bg-violet-200",
-      "bg-indigo-200",
-      "bg-sky-200",
-      "bg-rose-200",
+      "bg-gradient-to-r from-red-200 via-red-300 to-red-400",
+      "bg-gradient-to-r from-green-200 via-green-300 to-green-400",
+      "bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400",
+      "bg-gradient-to-r from-yellow-200 via-yellow-300 to-yellow-400",
+      "bg-gradient-to-r from-pink-200 via-pink-300 to-pink-400",
+      "bg-gradient-to-r from-violet-200 via-violet-300 to-violet-400",
+      "bg-gradient-to-r from-indigo-200 via-indigo-300 to-indigo-400",
+      "bg-gradient-to-r from-sky-200 via-sky-300 to-sky-400",
+      "bg-gradient-to-r from-rose-200 via-rose-300 to-rose-400",
     ];
     const sum = id.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
     const colorIndex = sum % colors.length;
@@ -144,7 +144,7 @@ export default function FoodCard({
           className={`w-80 h-96 border rounded-xl shadow-md hover:shadow-xl duration-200 relative ${bgColor}`}
         >
           {loading && (
-            <div className="absolute inset-0 flex items-center justify-center rounded-xl bg-opacity-80 bg-black">
+            <div className="absolute inset-0 flex items-center justify-center rounded-xl bg-opacity-70 bg-black">
               <Spinner />
             </div>
           )}
